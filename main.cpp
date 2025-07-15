9d8476c18d77069c3c934c060280cfc260a9656d
kooliha <a.koolinchenko@gmail.com>
2025-07-15 20:26:08 +0100

    Update MAX7219 segment mapping and control logic. Tested. All works

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ struct Max7219
     SpiHandle* spi;
     GPIO cs;
 
-    void Init(SpiHandle* spi_handle)
+    void Init(SpiHandle* spi_handle, Pin cs_pin)
     {
         spi = spi_handle;
-        cs.Init(D10, GPIO::Mode::OUTPUT);
+        cs.Init(cs_pin, GPIO::Mode::OUTPUT);
         cs.Write(true);
 
         // MAX7219 init sequence
@@ -34,26 +34,47 @@ struct Max7219
 };
 
 DaisySeed hw;
-Max7219 max;
+SpiHandle spi;
+Max7219 LedDriver;
 
 int main(void)
 {
     hw.Configure();
     hw.Init();
-    hw.spi.Init(); // Use default SPI settings
 
-    max.Init(&hw.spi);
+    // SPI configuration for Daisy Seed rev 7
+    SpiHandle::Config spi_cfg;
+    spi_cfg.periph = SpiHandle::Config::Peripheral::SPI_1;
+    spi_cfg.mode   = SpiHandle::Config::Mode::MASTER;
+    spi_cfg.direction = SpiHandle::Config::Direction::TWO_LINES_TX_ONLY;
+    spi_cfg.datasize  = 8;
+    spi_cfg.clock_polarity  = SpiHandle::Config::ClockPolarity::LOW;
+    spi_cfg.clock_phase     = SpiHandle::Config::ClockPhase::ONE_EDGE;
+    spi_cfg.nss             = SpiHandle::Config::NSS::SOFT;
+    spi_cfg.baud_prescaler  = SpiHandle::Config::BaudPrescaler::PS_64;
+    spi_cfg.pin_config.sclk = daisy::seed::D8;   // SCK
+    spi_cfg.pin_config.miso = daisy::seed::D9;   // MISO (not used for MAX7219)
+    spi_cfg.pin_config.mosi = daisy::seed::D10;  // MOSI
 
-    while(1)
-    {
-        // Blink LED 1 ON, LED 2 OFF
-        max.Send(1, 0x01); // LED 1 ON (row 1, first column)
-        max.Send(2, 0x00); // LED 2 OFF (row 2, all columns off)
-        hw.Delay(500);
+    spi.Init(spi_cfg);
 
-        // Blink LED 1 OFF, LED 2 ON
-        max.Send(1, 0x00); // LED 1 OFF
-        max.Send(2, 0x01); // LED 2 ON (row 2, first column)
-        hw.Delay(500);
-    }
+    LedDriver.Init(&spi, daisy::seed::D7); // Use D7 for CS/LOAD
+
+while(1)
+{
+
+LedDriver.Send(1, 0x01); // G Dig0. Track 1 Play
+LedDriver.Send(1, 0x02); // F Dig0. Track 1 Rec
+LedDriver.Send(1, 0x04); // E Dig0. Track 2 Play
+LedDriver.Send(1, 0x08); // D Dig0. Track 2 Rec
+LedDriver.Send(1, 0x10); // C Dig0. Track 3 Play
+LedDriver.Send(1, 0x20); // B Dig0. Track 3 Rec
+LedDriver.Send(1, 0x40); // A Dig0. Track 4 Play        
+LedDriver.Send(1, 0x80); // DP Dig0. Track 4 Rec
+LedDriver.Send(2, 0x80); // DP Dig1. Track 5 Play
+LedDriver.Send(2, 0x40); // A Dig1 Track 5 Rec
+
+    
+}
+
 }
